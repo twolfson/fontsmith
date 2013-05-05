6d0637ff00f8f4769785cc4db6f7ff9b74118667
Todd Wolfson <todd@twolfson.com>
2013-05-05 05:00:03 -0700

    Generating file results, wheee

--- test/fontsmith_test_content.js
+++ test/fontsmith_test_content.js
@@ -1,4 +1,5 @@
 var assert = require('assert'),
+    fs = require('fs'),
     fontsmith = require('../lib/fontsmith');
 module.exports = {
   "An array of SVGs": function () {
@@ -21,9 +22,26 @@ module.exports = {
     });
   },
   "generates some fonts": function () {
+    // DEV: Write out fonts to files
+    var fonts = this.results.fonts;
+    if (true) {
+      try { fs.mkdirSync(__dirname + '/actual_files'); } catch (e) {}
+      fs.writeFileSync(__dirname + '/actual_files/font.svg', fonts.svg, 'binary');
+      fs.writeFileSync(__dirname + '/actual_files/font.ttf', fonts.ttf, 'binary');
+      fs.writeFileSync(__dirname + '/actual_files/font.otf', fonts.otf, 'binary');
+      fs.writeFileSync(__dirname + '/actual_files/font.woff', fonts.woff, 'binary');
+      fs.writeFileSync(__dirname + '/actual_files/font.eot', fonts.eot, 'binary');
+      fs.writeFileSync(__dirname + '/actual_files/font.dev.svg', fonts['dev-svg'], 'binary');
+    }
+
     // TODO: Assert against expected_files
+    // console.log(fonts);
   },
   "generates an mapping from files to unicode characters": function () {
-    // TODO: Assert against expected_files
+    var map = this.results.map;
+    // DEV: Hex equivalent of 'e000'
+    assert.strictEqual(map.building_block, 57344);
+    assert.strictEqual(map.moon, 57345);
+    assert.strictEqual(map.eye, 57346);
   }
 };
