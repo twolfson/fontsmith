116a69c33ef5d96273214e692607e379eaf5f0a6
Todd Wolfson <todd@twolfson.com>
2013-05-04 23:01:58 -0700

    Wrote out as much of test as possible yet

--- test/fontsmith_test_content.js
+++ test/fontsmith_test_content.js
@@ -1,10 +1,28 @@
+var assert = require('assert'),
+    fontsmith = require('../lib/fontsmith');
 module.exports = {
   "An array of SVGs": function () {
+    // Set up our list of SVGs
+    this.files = [
+      __dirname + '/test_files/eye.svg',
+      __dirname + '/test_files/moon.svg',
+      __dirname + '/test_files/building_block.svg'
+    ];
   },
-  "rendered with fontsmith": function () {
+  "rendered with fontsmith": function (done) {
+    // Render with fontsmith
+    var params = {src: this.files},
+        that = this;
+    fontsmith(params, function (err, results) {
+      // Save the results for later and callback
+      that.results = results;
+      cb(err);
+    });
   },
   "generates some fonts": function () {
+    // TODO: Assert against expected_files
   },
   "generates an mapping from files to unicode characters": function () {
+    // TODO: Assert against expected_files
   }
 };
