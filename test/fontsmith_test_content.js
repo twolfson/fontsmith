68c957f73dec66ca09bb53fd49e7c289a99cd874
Todd Wolfson <todd@twolfson.com>
2013-05-04 22:57:17 -0700

    Adding in test files

---
+++ test/fontsmith_test_content.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "An array of SVGs": function () {
+  },
+  "rendered with fontsmith": function () {
+  },
+  "generates some fonts": function () {
+  },
+  "generates an mapping from files to unicode characters": function () {
+  }
+};
